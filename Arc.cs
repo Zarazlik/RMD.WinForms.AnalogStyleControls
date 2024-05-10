808b3046aff816472e2e3d7b3dd1ffc710304f9a
Matvei Riabikov <Zarazlik@Gmail.com>
2024-05-10 22:33:03 +0400

    Chronometer: Added relative rendering and size response

---
+++ Arc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMD.WinForms.AnalogStyleControls
+{
+	public struct Arc
+	{
+		public float StartRadian { get; set; }
+		public float EndRadian { get; private set; }
+		public float SweepRadian
+		{
+			get { return sweepRadian; }
+			set
+			{
+				sweepRadian = value;
+				EndRadian = StartRadian + sweepRadian;
+			}
+		}
+		float sweepRadian;
+
+		public Arc()
+		{
+
+		}
+
+		public Arc(float startRadian, float sweepRadian)
+		{
+			StartRadian = startRadian;
+			SweepRadian = sweepRadian;
+		}
+	}
+}
--- Chronometer.cs
+++ Chronometer.cs
@@ -7,11 +7,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing.Drawing2D;
+using System.Runtime.Serialization;
 
 namespace RMD.WinForms.AnalogStyleControls
 {
 	public class Chronometer : Control
 	{
+		#region Propertyes/Variable
 		public float Value
 		{
 			get { return value; }
@@ -35,36 +37,54 @@ namespace RMD.WinForms.AnalogStyleControls
 			}
 		}
 		float value;
-
 		public float NeedleAngle { get; private set; }
 
-		[Category("Behavior")]
-		public float StartNeedleAngle { get; set; }
-		[Category("Behavior")]
-		public float EndNeedleAngle { get; private set; }
-		[Category("Behavior")]
+		Arc needleArc;
+		[Category("Positioning")]
+		public float StartNeedleAngle 
+		{ 
+			get { return needleArc.StartRadian; }
+			set { needleArc.StartRadian = value; }
+		}
+		[Category("Positioning")]
+		public float EndNeedleAngle 
+		{ 
+			get { return needleArc.EndRadian; }
+		}
+		[Category("Positioning")]
 		public float SweepNeedleAngle
 		{
-			get { return sweepNeedleAngle; }
-			set 
-			{ 
-				sweepNeedleAngle = value;
-				EndNeedleAngle = StartNeedleAngle + sweepNeedleAngle;
-			}
+			get { return needleArc.SweepRadian; }
+			set { needleArc.SweepRadian = value; }
 		}
-		float sweepNeedleAngle;
 
-		[Category("Behavior")]
+		[Category("Scale")]
 		public float MinValue { get; set; }		
-		[Category("Behavior")]
+		[Category("Scale")]
 		public float MaxValue { get; set; }
 
-		[Category("Behavior")]
+		[Category("Scale")]
 		public float ScaleRodsStep { get; set; }
-		[Category("Behavior")]
+		[Category("Scale")]
 		public int AccentScaleRodsStep { get; set; }
-		[Category("Behavior")]
+		[Category("Scale")]
 		public int SubAccentScaleRodsStep { get; set; }
+
+		[Category("Scale")]
+		public Point[] ColorScaleLineValues { get; set; }
+		[Category("Scale")]
+		public Color[] ColorScaleLineColors { get; set; }
+
+		[Category("Scale")]
+		float IndentNeedle { get; set; } = 12;
+		[Category("Scale")]
+		float IndentRods { get; set; } = 10;
+		[Category("Scale")]
+		float IndentAccentRods { get; set; } = 15;
+		[Category("Scale")]
+		float IndentSubAccentRods { get; set; } = 15;
+
+		#endregion
 
 		public Chronometer() 
 		{ 
@@ -73,22 +93,22 @@ namespace RMD.WinForms.AnalogStyleControls
 
 		protected override void OnPaint(PaintEventArgs e)
 		{
+			base.OnPaint(e);
+
+			#region Preparation
 			Graphics g = e.Graphics;
 			g.SmoothingMode = SmoothingMode.HighQuality;
 
 			PointF center = new PointF(this.Width / 2, this.Height / 2);
+			
+			int maxRadius = Math.Min((this.Width / 2) - 3, (this.Height / 2) - 3);
+			#endregion
 
-			DrawArcLine(new Pen(Color.Blue, 5), center, 175, StartNeedleAngle, 29);
-			DrawArcLine(new Pen(Color.Green, 5), center, 175, StartNeedleAngle + 31, 90);
-			DrawArcLine(new Pen(Color.Orange, 5), center, 175, StartNeedleAngle + 122, 28);
-			DrawArcLine(new Pen(Color.Red, 5), center, 175, StartNeedleAngle + 151, 29);
-			DrawRods();
+			DrawColoredScaleLines();
+			DrawScale();
+			DrawNeedle();
 
-			g.FillEllipse(new SolidBrush(Color.Red), center.X - 5, center.Y - 5,  10, 10);
-			g.DrawLine(new Pen(Color.Red, 2.5f), center, GetPointOnCercle(center, 188, NeedleAngle));
-
-			DrawArcLine(new Pen(Color.Black), center, 200, StartNeedleAngle, SweepNeedleAngle);
-
+			#region Methods
 
 			PointF GetPointOnCercle(PointF center, float radius, float angle)
 			{
@@ -100,6 +120,47 @@ namespace RMD.WinForms.AnalogStyleControls
 				return new PointF(x, y);
 			}
 
+			void DrawScale()
+			{
+				DrawRods();
+				DrawArcLine(new Pen(Color.Black), center, maxRadius, StartNeedleAngle, SweepNeedleAngle);
+				
+				void DrawRods()
+				{
+					uint rodsCount = (uint)((MaxValue - MinValue) / ScaleRodsStep);
+					float step = SweepNeedleAngle / rodsCount;
+
+					for (int i = 0, accent = AccentScaleRodsStep, subAccent = SubAccentScaleRodsStep; i <= rodsCount; i++, accent++, subAccent++)
+					{
+						if (accent == AccentScaleRodsStep)
+						{
+							g.DrawLine(new Pen(Color.Black, 3), GetPointOnCercle(center, maxRadius, StartNeedleAngle + (step * i)), GetPointOnCercle(center, maxRadius - IndentAccentRods, StartNeedleAngle + (step * i)));
+							accent = 0;
+
+							if (subAccent == SubAccentScaleRodsStep)
+							{
+								subAccent = 0;
+							}
+						}
+						else if (subAccent == SubAccentScaleRodsStep)
+						{
+							g.DrawLine(new Pen(Color.Black), GetPointOnCercle(center, maxRadius, StartNeedleAngle + (step * i)), GetPointOnCercle(center, maxRadius - IndentSubAccentRods, StartNeedleAngle + (step * i)));
+							subAccent = 0;
+						}
+						else
+						{
+							g.DrawLine(new Pen(Color.Black), GetPointOnCercle(center, maxRadius, StartNeedleAngle + (step * i)), GetPointOnCercle(center, maxRadius - IndentRods, StartNeedleAngle + (step * i)));
+						}
+					}
+				}
+			}
+
+			void DrawNeedle()
+			{
+				g.FillEllipse(new SolidBrush(Color.Red), center.X - 5, center.Y - 5, 10, 10);
+				g.DrawLine(new Pen(Color.Red, 2.5f), center, GetPointOnCercle(center, maxRadius - IndentNeedle, NeedleAngle));
+			}
+
 			void DrawArcLine(Pen pen, PointF center, float radius, float startAngle, float sweepAngle)
 			{
 				RectangleF rect = new RectangleF(center.X - radius, center.Y - radius, 2 * radius, 2 * radius);
@@ -107,35 +168,21 @@ namespace RMD.WinForms.AnalogStyleControls
 				g.DrawArc(pen, rect, startAngle, sweepAngle);
 			}
 
-			void DrawRods()
+			void DrawColoredScaleLines()
 			{
-				uint rodsCount = (uint)((MaxValue - MinValue) / ScaleRodsStep);
-				float step = SweepNeedleAngle / rodsCount;
-
-				for (int i = 0, accent = AccentScaleRodsStep, subAccent = SubAccentScaleRodsStep; i <= rodsCount; i++, accent++, subAccent++)
-				{
-					if (accent == AccentScaleRodsStep) 
-					{
-						g.DrawLine(new Pen(Color.Black, 3), GetPointOnCercle(center, 200, StartNeedleAngle + (step * i)), GetPointOnCercle(center, 185, StartNeedleAngle + (step * i)));
-						accent = 0;
-
-						if (subAccent == SubAccentScaleRodsStep)
-						{
-							subAccent = 0;
-						}
-					}
-					else if (subAccent == SubAccentScaleRodsStep)
-					{
-						g.DrawLine(new Pen(Color.Black), GetPointOnCercle(center, 200, StartNeedleAngle + (step * i)), GetPointOnCercle(center, 185, StartNeedleAngle + (step * i)));
-						subAccent = 0;
-					}
-					else
-					{
-						g.DrawLine(new Pen(Color.Black), GetPointOnCercle(center, 200, StartNeedleAngle + (step * i)), GetPointOnCercle(center, 190, StartNeedleAngle + (step * i)));
-					}
-					
-				}
+				DrawArcLine(new Pen(Color.Blue, 5), center, 175, StartNeedleAngle, 29);
+				DrawArcLine(new Pen(Color.Green, 5), center, 175, StartNeedleAngle + 31, 90);
+				DrawArcLine(new Pen(Color.Orange, 5), center, 175, StartNeedleAngle + 122, 28);
+				DrawArcLine(new Pen(Color.Red, 5), center, 175, StartNeedleAngle + 151, 29);
 			}
+
+			#endregion
+		}
+
+		protected override void OnSizeChanged(EventArgs e)
+		{
+			base.OnSizeChanged(e);
+			Refresh();
 		}
 
 		private float GetNeedleAngel(float minAngle, float maxAngle, float minValue, float maxValue)
