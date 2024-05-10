aa505555a584fc13031544e4bd92a441fd739d60
Matvei Riabikov <Zarazlik@Gmail.com>
2024-05-11 03:30:30 +0400

    Chronometer: Optimized display of static elements. The scale is drawn when the control is resized

--- Chronometer.cs
+++ Chronometer.cs
@@ -14,6 +14,8 @@ namespace RMD.WinForms.AnalogStyleControls
 	public class Chronometer : Control
 	{
 		#region Propertyes/Variable
+
+		#region Value
 		public float Value
 		{
 			get { return value; }
@@ -38,7 +40,10 @@ namespace RMD.WinForms.AnalogStyleControls
 		}
 		float value;
 		public float NeedleAngle { get; private set; }
+		float valueAngeleStep;
+		#endregion
 
+		#region Positioning
 		Arc needleArc = new Arc(150, 180);
 		[Category("Positioning")]
 		public float StartNeedleAngle 
@@ -57,11 +62,20 @@ namespace RMD.WinForms.AnalogStyleControls
 			get { return needleArc.SweepRadian; }
 			set { needleArc.SweepRadian = value; }
 		}
+		#endregion
 
+		#region Scale
 		[Category("Scale")]
 		public float MinValue { get; set; } = 0;
 		[Category("Scale")]
 		public float MaxValue { get; set; } = 100;
+
+		[Category("Scale")]
+		public Color ScaleColor {  get; set; } = Color.Black;
+		[Category("Scale")]
+		public Color NeedleColor { get; set; } = Color.Red;
+		[Category("Scale")]
+		public float  NeedleWidth { get; set; }= 2.5f;
 
 		[Category("Scale")]
 		public float ScaleRodsStep { get; set; } = 1;
@@ -99,8 +113,12 @@ namespace RMD.WinForms.AnalogStyleControls
 		public float IndentSubAccentRods { get; set; } = 15;
 		[Category("Scale")]
 		public float ColorScaleLineWidth { get; set; } = 5;
+		#endregion
 
-		float valueAngeleStep; 
+		Bitmap scaleBackground;
+		PointF center;
+		int maxRadius;
+		bool redrawBackground = true;
 
 		#endregion
 
@@ -114,101 +132,122 @@ namespace RMD.WinForms.AnalogStyleControls
 			base.OnPaint(e);
 
 			#region Preparation
+
 			Graphics g = e.Graphics;
 			g.SmoothingMode = SmoothingMode.HighQuality;
 
 			SetValueAngeleStep();
 
-			PointF center = new PointF(this.Width / 2, this.Height / 2);
-			
-			int maxRadius = Math.Min((this.Width / 2) - 3, (this.Height / 2) - 3);
 			#endregion
 
-			DrawColoredScaleLines();
-			DrawScale();
-			DrawNeedle();
-
-			#region Methods
-
-			PointF GetPointOnCercle(PointF center, float radius, float angle)
+			if (redrawBackground)
 			{
-				float alphaRadians = (float)(angle * Math.PI / 180);
-
-				float x = (float)(center.X + radius * Math.Cos(alphaRadians));
-				float y = (float)(center.Y + radius * Math.Sin(alphaRadians));
-
-				return new PointF(x, y);
+				RedrawScaleBackground();
+				redrawBackground = false;
 			}
 
-			void DrawScale()
-			{
-				DrawRods();
-				DrawArcLine(new Pen(Color.Black), center, maxRadius, StartNeedleAngle, SweepNeedleAngle);
-				
-				void DrawRods()
-				{
-					uint rodsCount = (uint)((MaxValue - MinValue) / ScaleRodsStep);
-					float step = SweepNeedleAngle / rodsCount;
-
-					for (int i = 0, accent = AccentScaleRodsStep, subAccent = SubAccentScaleRodsStep; i <= rodsCount; i++, accent++, subAccent++)
-					{
-						if (accent == AccentScaleRodsStep)
-						{
-							g.DrawLine(new Pen(Color.Black, 3), GetPointOnCercle(center, maxRadius, StartNeedleAngle + (step * i)), GetPointOnCercle(center, maxRadius - IndentAccentRods, StartNeedleAngle + (step * i)));
-							accent = 0;
-
-							if (subAccent == SubAccentScaleRodsStep)
-							{
-								subAccent = 0;
-							}
-						}
-						else if (subAccent == SubAccentScaleRodsStep)
-						{
-							g.DrawLine(new Pen(Color.Black), GetPointOnCercle(center, maxRadius, StartNeedleAngle + (step * i)), GetPointOnCercle(center, maxRadius - IndentSubAccentRods, StartNeedleAngle + (step * i)));
-							subAccent = 0;
-						}
-						else
-						{
-							g.DrawLine(new Pen(Color.Black), GetPointOnCercle(center, maxRadius, StartNeedleAngle + (step * i)), GetPointOnCercle(center, maxRadius - IndentRods, StartNeedleAngle + (step * i)));
-						}
-					}
-				}
-			}
-
-			void DrawNeedle()
-			{
-				g.FillEllipse(new SolidBrush(Color.Red), center.X - 5, center.Y - 5, 10, 10);
-				g.DrawLine(new Pen(Color.Red, 2.5f), center, GetPointOnCercle(center, maxRadius - IndentNeedle, NeedleAngle));
-			}
-
-			void DrawArcLine(Pen pen, PointF center, float radius, float startAngle, float sweepAngle)
-			{
-				RectangleF rect = new RectangleF(center.X - radius, center.Y - radius, 2 * radius, 2 * radius);
-
-				g.DrawArc(pen, rect, startAngle, sweepAngle);
-			}
-
-			void DrawColoredScaleLines()
-			{
-				for (int i = 0; i < ColorScaleLineColors.Length && i < ColorScaleLineValues.Length; i++)
-				{
-					DrawArcLine(
-						new Pen(ColorScaleLineColors[i], ColorScaleLineWidth),
-						center,
-						maxRadius - IndentColorScaleLine,
-						GetAngelOfValue(ColorScaleLineValues[i].X),
-						(ColorScaleLineValues[i].Y - ColorScaleLineValues[i].X) * valueAngeleStep
-						);
-				}
-			}
-
-			#endregion
+			g.DrawImage(scaleBackground, new Point(0,0));
+			DrawNeedle(g, center, maxRadius);
 		}
 
 		protected override void OnSizeChanged(EventArgs e)
 		{
 			base.OnSizeChanged(e);
+			redrawBackground = true;
 			Refresh();
+		}
+
+		#region Drawning
+
+		protected void RedrawScaleBackground()
+		{
+			center = new PointF(this.Width / 2, this.Height / 2);
+			maxRadius = Math.Min((this.Width / 2) - 3, (this.Height / 2) - 3);
+
+			scaleBackground = new Bitmap(this.Width, this.Height);
+
+			using (Graphics g = Graphics.FromImage(scaleBackground)) 
+			{
+				g.SmoothingMode = SmoothingMode.HighQuality;
+
+				DrawColoredScaleLines(g, center, maxRadius);
+				DrawScale(g, center, maxRadius);
+			}
+		}
+
+		protected void DrawScale(Graphics g, PointF center, float maxRadius)
+		{
+			DrawRods();
+			DrawArcLine(g, new Pen(ScaleColor), center, maxRadius, StartNeedleAngle, SweepNeedleAngle);
+
+			void DrawRods()
+			{
+				uint rodsCount = (uint)((MaxValue - MinValue) / ScaleRodsStep);
+				float step = SweepNeedleAngle / rodsCount;
+
+				for (int i = 0, accent = AccentScaleRodsStep, subAccent = SubAccentScaleRodsStep; i <= rodsCount; i++, accent++, subAccent++)
+				{
+					if (accent == AccentScaleRodsStep)
+					{
+						g.DrawLine(new Pen(ScaleColor, 3), GetPointOnCercle(center, maxRadius, StartNeedleAngle + (step * i)), GetPointOnCercle(center, maxRadius - IndentAccentRods, StartNeedleAngle + (step * i)));
+						accent = 0;
+
+						if (subAccent == SubAccentScaleRodsStep)
+						{
+							subAccent = 0;
+						}
+					}
+					else if (subAccent == SubAccentScaleRodsStep)
+					{
+						g.DrawLine(new Pen(ScaleColor), GetPointOnCercle(center, maxRadius, StartNeedleAngle + (step * i)), GetPointOnCercle(center, maxRadius - IndentSubAccentRods, StartNeedleAngle + (step * i)));
+						subAccent = 0;
+					}
+					else
+					{
+						g.DrawLine(new Pen(ScaleColor), GetPointOnCercle(center, maxRadius, StartNeedleAngle + (step * i)), GetPointOnCercle(center, maxRadius - IndentRods, StartNeedleAngle + (step * i)));
+					}
+				}
+			}
+		}
+
+		protected void DrawNeedle(Graphics g, PointF center, float maxRadius)
+		{
+			g.FillEllipse(new SolidBrush(NeedleColor), center.X - 5, center.Y - 5, 10, 10);
+			g.DrawLine(new Pen(Color.Red, NeedleWidth), center, GetPointOnCercle(center, maxRadius - IndentNeedle, NeedleAngle));
+		}
+
+		protected void DrawColoredScaleLines(Graphics g, PointF center, float maxRadius)
+		{
+			for (int i = 0; i < ColorScaleLineColors.Length && i < ColorScaleLineValues.Length; i++)
+			{
+				DrawArcLine(
+					g,
+					new Pen(ColorScaleLineColors[i], ColorScaleLineWidth),
+					center,
+					maxRadius - IndentColorScaleLine,
+					GetAngelOfValue(ColorScaleLineValues[i].X),
+					(ColorScaleLineValues[i].Y - ColorScaleLineValues[i].X) * valueAngeleStep
+					);
+			}
+		}
+
+		protected void DrawArcLine(Graphics g, Pen pen, PointF center, float radius, float startAngle, float sweepAngle)
+		{
+			RectangleF rect = new RectangleF(center.X - radius, center.Y - radius, 2 * radius, 2 * radius);
+
+			g.DrawArc(pen, rect, startAngle, sweepAngle);
+		}
+
+		#endregion
+
+		PointF GetPointOnCercle(PointF center, float radius, float angle)
+		{
+			float alphaRadians = (float)(angle * Math.PI / 180);
+
+			float x = (float)(center.X + radius * Math.Cos(alphaRadians));
+			float y = (float)(center.Y + radius * Math.Sin(alphaRadians));
+
+			return new PointF(x, y);
 		}
 
 		private float GetAngelOfValue(float value)
